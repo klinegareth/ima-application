ebfe9589625e60faa51e644123d32b18220dfb56
klinegareth <klinegareth@gmail.com>
2022-03-03 11:43:29 -0500

    Updated package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -12,11 +12,11 @@ export default defineConfig({
 	  })
 	],
 	build: {
-		chunkSizeWarningLimit: 1600,
+		chunkSizeWarningLimit: 1600
 	},
 	server: {
 		https: true,
-		host: 0.0.0.0,
+		host: '0.0.0.0',
 		port: 8080,
 		strictPort: true
 	}
